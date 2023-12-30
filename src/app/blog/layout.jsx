298fff34793b810ad8cb651014662f04025022b4
Mesut <semihturgutlu@gmail.com>
2023-12-30 11:56:25 +0100

    Layaout has been editted

---
+++ src/app/blog/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogLayout = ({children}) => {
+  return (
+    <div> 
+      <h1>Hi this is Blog</h1>
+      {children}</div>
+  )
+}
+
+export default BlogLayout
