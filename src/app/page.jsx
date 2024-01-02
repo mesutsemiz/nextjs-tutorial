a8061d414786229ab168e4edd00587cd8039bc53
Mesut <semihturgutlu@gmail.com>
2024-01-02 23:58:10 +0100

    internal image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,9 @@ import Hero from "../../public/hero.png"
 export default function Home() {
   return (
    <div className={style.container}>
-    <Image src="https://images.pexels.com/photos/19343942/pexels-photo-19343942/free-photo-of-stadt-kunst-gebaude-architekt.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" width={600} height={500} alt='hero' className={style.img}/>
+    <Image src={Hero} alt='hero' className={style.img}/>
+    {/* <Image src="https://images.pexels.com/photos/19343942/pexels-photo-19343942/free-photo-of-stadt-kunst-gebaude-architekt.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" width={600} height={500} alt='hero' className={style.img}/> */}
+
    </div>
   )
 }
