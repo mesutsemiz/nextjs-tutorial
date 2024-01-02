5a6ccada495fc3aa991bc1b2d674b28b6c8a512d
Mesut <semihturgutlu@gmail.com>
2024-01-02 23:54:04 +0100

    external image upload

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
-import Image from 'next/image'
-import style from './page.module.css'
+import Image from 'next/image';
+import style from './page.module.css';
+import Hero from "../../public/hero.png"
+
 
 export default function Home() {
   return (
    <div className={style.container}>
-    home
+    <Image src="https://images.pexels.com/photos/19343942/pexels-photo-19343942/free-photo-of-stadt-kunst-gebaude-architekt.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" width={600} height={500} alt='hero' className={style.img}/>
    </div>
   )
 }
