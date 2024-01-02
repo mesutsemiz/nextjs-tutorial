371c2d5e5c5a32ff5766276a53051a5ca6e7a431
Mesut <semihturgutlu@gmail.com>
2024-01-02 23:42:22 +0100

    image adding -1

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import style from './footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
 
   return (
     <div  className={style.container}>
           <div>©2023 Lamamia. All rights reserved.</div>
-          <div></div>
+          <div>
+            <div className={style.social}>
+            <Image src="/1.png"  width={15} height={15} className={style.icon} alt='LAmaDev'/>
+            <Image src="/2.png"  width={15} height={15} className={style.icon} alt='LAmaDev'/>
+            <Image src="/3.png"  width={15} height={15} className={style.icon} alt='LAmaDev'/>
+            <Image src="/4.png"  width={15} height={15} className={style.icon} alt='LAmaDev'/>
+            </div>
+          </div>
     </div>
   )
 }
