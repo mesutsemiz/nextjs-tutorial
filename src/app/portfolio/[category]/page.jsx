41cbd3f2869e49deb01f26bd6569911de3c04fa6
Mesut <semihturgutlu@gmail.com>
2024-01-04 21:37:54 +0100

    contact page and styling is done

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import style from "./page.module.css";
+import Button from "@/components/Button/Button";
+import Image from "next/image";
 
-const  Category = () => {
+const Category = ({ params }) => {
+  console.log({ params });
   return (
-    <div> Category </div>
-  )
-}
+    <div className={style.container}>
+      <h1 className={style.catTitle}>{params.category}</h1>
+      <div className={style.item}>
+        <div className={style.content}>
+          <h1 className={style.title}>test</h1>
+          <p className={style.description}>description</p>
+          <Button text="See more" url="#" />
+        </div>
+        <div className={style.imgContainer}>
+          <Image
+            className={style.img}
+            src="https://images.pexels.com/photos/12944595/pexels-photo-12944595.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            fill={true}
+            alt=""
+          />
+        </div>
+      </div>
+      <div className={style.item}>
+        <div className={style.content}>
+          <h1 className={style.title}>test</h1>
+          <p className={style.description}>description</p>
+          <Button text="See more" url="#" />
+        </div>
+        <div className={style.imgContainer}>
+          <Image
+            className={style.img}
+            src="https://images.pexels.com/photos/12944595/pexels-photo-12944595.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            fill={true}
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Category 
+export default Category;
