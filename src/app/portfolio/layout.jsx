8d74ee51cdd55e425d7aea2d69db30e68c0e5106
Mesut <semihturgutlu@gmail.com>
2024-01-04 13:10:46 +0100

    portfolio page und styling

---
+++ src/app/portfolio/layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./page.module.css";
+
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <h1 className={style.mainTitle}>Our Works</h1>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
