1903496386dec755fa7188e4d325d9f418dbd3d2
Mesut <semihturgutlu@gmail.com>
2024-01-05 22:36:30 +0100

    data fetching from client side(useEffect)

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import style from './page.module.css'
+"use client";
+
+import React, { useEffect, useState } from "react";
+import style from "./page.module.css";
 
 const Dashboard = () => {
-  return (
-    <div className={style.container}> Dashboard</div>
-  )
-}
+  const [data, setData] = useState([]);
+  const [err, setErr] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-export default Dashboard
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true);
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        cache: "no-store",
+      });
+
+      if (!res.ok) {
+        setErr(true);
+      }
+
+      const data = await res.json()
+      setData(res.json());
+      setIsLoading(true);
+    };
+    getData()
+  }, []);
+
+  console.log(data)
+
+  return <div className={style.container}> Dashboard</div>;
+};
+
+export default Dashboard;
