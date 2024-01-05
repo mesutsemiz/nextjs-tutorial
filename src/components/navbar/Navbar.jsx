40038aec477d2a5ea1024f6bbb7cc2d8be6e7596
Mesut <semihturgutlu@gmail.com>
2024-01-05 17:16:00 +0100

    dark mode toggle comp and styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,55 +1,66 @@
-"use client"
-import Link from 'next/link'
-import React from 'react'
-import style from './navbar.module.css'
-
+"use client";
+import Link from "next/link";
+import React from "react";
+import style from "./navbar.module.css";
+import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 
 const links = [
-    {
-      id: 1,
-      title: "Home",
-      url: "/",
-    },
-    {
-      id: 2,
-      title: "Portfolio",
-      url: "/portfolio",
-    },
-    {
-      id: 3,
-      title: "Blog",
-      url: "/blog",
-    },
-    {
-      id: 4,
-      title: "About",
-      url: "/about",
-    },
-    {
-      id: 5,
-      title: "Contact",
-      url: "/contact",
-    },
-    {
-      id: 6,
-      title: "Dashboard",
-      url: "/dashboard",
-    },
-  ];
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 4,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
 
 const Navbar = () => {
-
   return (
-    <div  className={style.container}>
-        <Link href="/" className={style.logo}>Lamamia</Link>
-        <div className={style.links} >
-            {links.map(link=>(
-                <Link key={link.id} href={link.url} className={style.link}>{link.title}</Link>
-            ))}
-            <button onClick={()=>{console.log("logged out")}} className={style.logout}>logout</button>
-        </div>
+    <div className={style.container}>
+      <Link href="/" className={style.logo}>
+        Lamamia
+      </Link>
+      <div className={style.links}>
+        <DarkModeToggle />
+        {links.map((link) => (
+          <Link key={link.id} href={link.url} className={style.link}>
+            {link.title}
+          </Link>
+        ))}
+        <button
+          onClick={() => {
+            console.log("logged out");
+          }}
+          className={style.logout}
+        >
+          logout
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
