9cc2e6fe4d3e2ca8530b3174fcb1302ee58f77a0
Mesut <semihturgutlu@gmail.com>
2024-01-05 17:44:26 +0100

    using Context , toggle button has been created

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,18 +1,19 @@
-"use client"
+"use client";
 
 import { createContext, useState } from "react";
 
-export const ThemeContext = createContext()
+export const ThemeContext = createContext();
 
+export const ThemeProvider = ({ children }) => {
+  const [mode, setMode] = useState("dark");
 
-export const ThemeProvider =({})=>{
-    const [mode , setMode] = useState("dark")
+  const toggle = () => {
+    setMode((prev) => (prev === "dark" ? "light" : "dark"));
+  };
 
-    const toggle = ()=>{
-        setMode((prev)=>(prev==="dark" ? "light" : "dark"))
-    }
-}
-
-<ThemeContext.Provider value={{toggle,mode}}>
-    {children}
-</ThemeContext.Provider>
+  return (
+    <ThemeContext.Provider value={{ toggle, mode }}>
+      <div className={`theme ${mode}`}>{children}</div>
+    </ThemeContext.Provider>
+  );
+};
