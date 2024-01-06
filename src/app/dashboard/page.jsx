4c6fd1a458e3d7195372813b224e70a5486ed617
Mesut <semihturgutlu@gmail.com>
2024-01-06 23:46:06 +0100

    instead of useEffect using SWR in next.js

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,29 +2,36 @@
 
 import React, { useEffect, useState } from "react";
 import style from "./page.module.css";
+import useSWR from "swr";
 
 const Dashboard = () => {
-  const [data, setData] = useState([]);
-  const [err, setErr] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  // const [data, setData] = useState([]);
+  // const [err, setErr] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const getData = async () => {
-      setIsLoading(true);
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-        cache: "no-store",
-      });
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     setIsLoading(true);
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  //       cache: "no-store",
+  //     });
 
-      if (!res.ok) {
-        setErr(true);
-      }
+  //     if (!res.ok) {
+  //       setErr(true);
+  //     }
 
-      const data = await res.json()
-      setData(res.json());
-      setIsLoading(true);
-    };
-    getData()
-  }, []);
+  //     const data = await res.json()
+  //     setData(res.json());
+  //     setIsLoading(true);
+  //   };
+  //   getData()
+  // }, []);
+
+  // console.log(data)
+
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+
+  const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
 
   console.log(data)
 
