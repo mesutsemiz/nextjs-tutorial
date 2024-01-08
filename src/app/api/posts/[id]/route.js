8864023cc984d995d37d5153f9c1064eaa374e62
Mesut <semihturgutlu@gmail.com>
2024-01-08 23:59:46 +0100

    post route is prepared

---
+++ src/app/api/posts/[id]/route.js
@@ -0,0 +1,20 @@
+import Post from "@/models/Post";
+import connect from "@/utils/db";
+import { NextResponse } from "next/server";
+
+
+export const GET =async (request, {params})=>{
+
+    const {id} =params
+
+    try {
+        await connect();
+
+        const post = await Post.findById(id);
+        return new NextResponse(JSON.stringify(post), {status:200});
+    } catch (error) {
+        return new NextResponse("DAtabase errorrr", {status:500});
+    }
+
+    
+}
