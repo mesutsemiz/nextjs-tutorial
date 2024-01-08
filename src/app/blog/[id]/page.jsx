8acaac9ffc2816985f2022d4c6b175fefed5193f
Mesut <semihturgutlu@gmail.com>
2024-01-09 00:16:18 +0100

    data turned yhe dynamic value

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -26,33 +26,28 @@ const BlogPost = async ({params}) => {
 {data.title}            
           </h1>
           <p className={styles.desc}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
-            excepturi veritatis iste quos, natus delectus eum corporis quo
-            voluptates ipsam, cumque fugit omnis nobis dolorum eveniet.
-            Recusandae facere quasi itaque!
+           {data.desc}
           </p>
           <div className={styles.author}>
             <Image
-              src="https://images.pexels.com/photos/12944595/pexels-photo-12944595.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-              alt=""
+              src={data.img}
               width={40}
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>John wick</span>
+            <span className={styles.username}>{data.username}</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
           <Image
-            src="https://images.pexels.com/photos/12944595/pexels-photo-12944595.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
+            src={data.img}
             fill={true}
             className={styles.image}
           />
         </div>
       </div>
       <div className={styles.content}>
-        <p className={styles.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo saepe, aperiam provident vero officia error quibusdam commodi deleniti voluptas tempora quae consectetur placeat dignissimos accusamus veritatis? Temporibus doloribus provident aliquam!</p>
+        <p className={styles.text}>{data.content}</p>
       </div>
     </div>
   );
